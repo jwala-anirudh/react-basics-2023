9b10447c5fc5db54b627ae867dc037594cc44087
Anirudh Jwala <anirudhsai2@gmail.com>
2023-06-29 21:04:17 +0530

    Building button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Alert from './components/Alert';
+import Button from './components/Button';
 import ListGroup from './components/ListGroup';
 
 function App() {
@@ -14,6 +15,15 @@ function App() {
       <Alert>
         Hello <span className="fw-bold">World</span>!
       </Alert>
+      {/* Button component */}
+      <Button onClick={() => console.log('Clicked')}>My Primary Button</Button>
+      <Button color="secondary" onClick={() => console.log('Clicked')}>
+        My Secondary Button
+      </Button>
+      {/* Gives compilation error by typescript for passing invalid color
+      <Button color="anirudh" onClick={() => console.log('Clicked')}>
+        My Invalid Color Button
+      </Button> */}
       {/* List group with rendering lists, conditional rendering, state, props and child-to-parent communication */}
       <ListGroup
         items={items}
