6f823a3fef07ec4d3ddb201e4a44e51a97939889
Anirudh Jwala <anirudhsai2@gmail.com>
2023-07-02 23:16:56 +0530

    Advanced networking concepts in react

--- src/networking/Networking.tsx
+++ src/networking/Networking.tsx
@@ -1,44 +1,74 @@
-import { useEffect, useState } from 'react';
-import axios, { CanceledError } from 'axios';
+import userService, { User } from './services/user-service';
 
-interface User {
-  id: number;
-  name: string;
-}
+// Custom hooks
+import useUsers from './hooks/useUsers';
 
 const Networking = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
-  useEffect(() => {
-    const controller = new AbortController();
+  const deleteUser = (user: User) => {
+    const orignialUsers = [...users];
+    setUsers(users.filter(u => u.id !== user.id));
 
-    setIsLoading(true);
+    userService.delete(user.id).catch(err => {
+      setError(err.message);
+      setUsers(orignialUsers);
+    });
+  };
 
-    axios
-      .get<User[]>('https://jsonplaceholder.typicode.com/users', {
-        signal: controller.signal
-      })
-      .then(res => {
-        setUsers(res.data);
-        setIsLoading(false);
-      })
+  const addUser = () => {
+    const orignialUsers = [...users];
+    const newUser = { id: 0, name: 'Josh Clieach Shep' };
+    setUsers([newUser, ...users]);
+
+    userService
+      .create(newUser)
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
       .catch(err => {
-        if (err instanceof CanceledError) return;
         setError(err.message);
-        setIsLoading(false);
+        setUsers(orignialUsers);
       });
+  };
 
-    return () => controller.abort();
-  }, []);
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + '!' };
+    setUsers(users.map(u => (u.id === user.id ? updatedUser : u)));
+
+    userService.updateUser(updatedUser).catch(err => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
 
   return (
-    <ul>
+    <ul className="list-group">
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary mb-3" onClick={addUser}>
+        Add
+      </button>
       {users.map(user => (
-        <li key={user.id}>{user.name}</li>
+        <li
+          key={user.id}
+          className="list-group-item d-flex justify-content-between"
+        >
+          {user.name}
+          <div>
+            <button
+              className="btn btn-outline-secondary mx-1"
+              onClick={() => updateUser(user)}
+            >
+              Update
+            </button>
+            <button
+              className="btn btn-outline-danger mx-1"
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
+          </div>
+        </li>
       ))}
     </ul>
   );
