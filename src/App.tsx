9ad1a13434ce0b828444198295c99ae4e4031f63
Anirudh Jwala <anirudhsai2@gmail.com>
2023-07-02 16:14:37 +0530

    Expense form

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import ExpandableText from './components/ExpandableText';
 import Form from './components/Form';
 import ExpenseList, { Expense } from './expense-tracker/components/ExpenseList';
 import ExpenseFilter from './expense-tracker/components/ExpenseFilter';
+import ExpenseForm from './expense-tracker/components/ExpenseForm';
+
+export const categories = ['Groceries', 'Utilities', 'Entertainment'];
 
 const App = () => {
   const items = ['New York', 'San Fracisco', 'Tokyo', 'London', 'Paris'];
@@ -68,6 +71,9 @@ const App = () => {
 
       <Form />
 
+      <div className="mb-5 mt-3">
+        <ExpenseForm />
+      </div>
       <div className="mt-3 md-3">
         <ExpenseFilter
           onSelectCategory={category => setSelectedCategory(category)}
